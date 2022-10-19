6d4cd7c31e33a02bcf822bc2550355697ed36967
Jeremy Clarke <gecko@exelo.tl>
2022-10-19 12:26:18 +0100

    Remove MPLv2 heading

--- src/tilengine.nim
+++ src/tilengine.nim
@@ -1,12 +1,4 @@
-#
-#  Tilengine - The 2D retro graphics engine with raster effects
-#  Copyright (C) 2015-2019 Marc Palacios Domenech <mailto:megamarc@hotmail.com>
-#  All rights reserved
-#
-#  This Source Code Form is subject to the terms of the Mozilla Public
-#  License, v. 2.0. If a copy of the MPL was not distributed with this
-#  file, You can obtain one at http://mozilla.org/MPL/2.0/.
-#
+# Tilengine - The 2D retro graphics engine with raster effects
 
 when defined(Windows):
   const libname* = "Tilengine.dll"
